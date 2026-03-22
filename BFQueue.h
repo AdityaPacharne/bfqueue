5538e0624b5d22ce2cc0d11551a39fdb92ad4990
Aditya Pacharne <48561911+AdityaPacharne@users.noreply.github.com>
2026-03-22 23:49:16 +0530

    Adding lock guards

--- BFQueue.h
+++ BFQueue.h
@@ -11,18 +11,22 @@ class BFQueue {
     BFQueue(const size_t capacity) { }
 
     void push(T t) {
+        std::lock_guard<std::mutex> lk(m);
         bfqueue.push(t);
     }
 
     void pop() {
+        std::lock_guard<std::mutex> lk(m);
         bfqueue.pop();
     }
 
     void emplace(T t) {
+        std::lock_guard<std::mutex> lk(m);
         bfqueue.emplace(std::move(t));
     }
 
     T* front() {
+        std::lock_guard<std::mutex> lk(m);
         if(bfqueue.empty()) return nullptr;
         return &bfqueue.front();
     }
