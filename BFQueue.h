ca1614ba62dc4e8ffbe943a635c2e5bddf040785
Aditya Pacharne <adityarpacharne@gmail.com>
2026-03-28 18:17:50 +0530

    Improved code

--- BFQueue.h
+++ BFQueue.h
@@ -1,33 +1,37 @@
 #include <queue>
 #include <mutex>
+#include <atomic>
+#include <vector>
 
 template <typename T>
 class BFQueue {
 
     public:
-    std::queue<T> bfqueue;
-    std::mutex m;
+    std::vector<T> bfqueue;
+    alignas(64) std::atomic<int> start{0};
+    alignas(64) std::atomic<int> end{0};
+    int capacity{};
 
-    BFQueue(const size_t capacity) { }
+    BFQueue(size_t initial_capacity) : bfqueue(initial_capacity), capacity(initial_capacity) {}
 
-    void push(T t) {
-        std::lock_guard<std::mutex> lk(m);
-        bfqueue.push(t);
+    void push(T t) noexcept {
+        int next = end.load(std::memory_order_relaxed) % capacity;
+        bfqueue[next] = std::move(t);
+        end.store(end.load(std::memory_order_relaxed) + 1, std::memory_order_release);
     }
 
-    void pop() {
-        std::lock_guard<std::mutex> lk(m);
-        bfqueue.pop();
+    void pop() noexcept {
+        start.fetch_add(1, std::memory_order_release);
     }
 
-    void emplace(T t) {
-        std::lock_guard<std::mutex> lk(m);
-        bfqueue.emplace(std::move(t));
+    void emplace(T t) noexcept {
+        push(std::move(t));
     }
 
-    T* front() {
-        std::lock_guard<std::mutex> lk(m);
-        if(bfqueue.empty()) return nullptr;
-        return &bfqueue.front();
+    T* front() noexcept {
+        int s = start.load(std::memory_order_relaxed);
+        int e = end.load(std::memory_order_acquire);
+        if(s == e) return nullptr;
+        return &bfqueue[s];
     }
 };
