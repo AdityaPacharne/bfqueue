97301bed2de69b3692b97c319a1ca30579056e9c
Aditya Pacharne <adityarpacharne@gmail.com>
2026-03-30 20:46:09 +0530

    Create cache of head for the consumer

--- BFQueue.h
+++ BFQueue.h
@@ -6,6 +6,7 @@ class BFQueue {
 
     public:
     alignas(64) std::atomic<size_t> tail{0};
+    size_t head_cache{};
     alignas(64) std::atomic<size_t> head{0};
     alignas(64) size_t capacity{};
     alignas(64) std::vector<T> bfqueue;
@@ -16,9 +17,9 @@ class BFQueue {
     {}
 
     void push(T t) noexcept {
-        size_t next = head.load(std::memory_order_relaxed) & (capacity - 1);
-        bfqueue[next] = std::move(t);
-        head.fetch_add(1, std::memory_order_release);
+        size_t h = head.load(std::memory_order_relaxed) & (capacity - 1);
+        bfqueue[h] = std::move(t);
+        head.store(h + 1, std::memory_order_release);
     }
 
     bool try_push(T t) noexcept {
@@ -28,8 +29,8 @@ class BFQueue {
     }
 
     void pop() noexcept {
-        size_t next = tail.load(std::memory_order_relaxed) & (capacity - 1);
-        tail.fetch_add(1, std::memory_order_release);
+        size_t t = tail.load(std::memory_order_relaxed);
+        tail.store(t + 1, std::memory_order_release);
     }
 
     void emplace(T t) noexcept {
@@ -44,26 +45,27 @@ class BFQueue {
 
     T* front() noexcept {
         size_t t = tail.load(std::memory_order_relaxed) & (capacity - 1);
-        size_t h = head.load(std::memory_order_acquire) & (capacity - 1);
-        if(t == h) return nullptr;
-        return &bfqueue[t];
+        if(t == head_cache) {
+            head_cache = head.load(std::memory_order_acquire) & (capacity - 1);
+        }
+        return (t == head_cache) ? nullptr : &bfqueue[t];
     }
 
     size_t size() {
-        size_t t = tail.load(std::memory_order_relaxed);
+        size_t t = tail.load(std::memory_order_acquire);
         size_t h = head.load(std::memory_order_acquire);
         return (h - t) & (capacity - 1);
     }
 
     bool empty() {
-        size_t t = tail.load(std::memory_order_relaxed);
+        size_t t = tail.load(std::memory_order_acquire);
         size_t h = head.load(std::memory_order_acquire);
         if(t == h) return true;
         return false;
     }
 
     bool filled() {
-        size_t t = tail.load(std::memory_order_relaxed);
+        size_t t = tail.load(std::memory_order_acquire);
         size_t h = head.load(std::memory_order_acquire);
         if((h - t) == capacity) return true;
         return false;
@@ -73,3 +75,5 @@ class BFQueue {
         return n == 0 ? 1 : 1 << (32 - __builtin_clz(n - 1));
     }
 };
+
+
